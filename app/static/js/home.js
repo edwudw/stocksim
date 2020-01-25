74095076ad9f2019dc5ffcab567851f51f4dfb84
edwudw <edward.webb9@outlook.com>
2020-01-25 20:27:39 +1100

    added responsive navbar

---
+++ app/static/js/home.js
@@ -0,0 +1,64 @@
+function showMenus() {
+    if ($(window).width() < 800) {
+        $('.nav-right').hide();
+        $('#hamburger').show();
+        $('nav img').hide();
+    } else {
+        $('nav img').show();
+        $('.nav-right').show();
+        $('#hamburger').hide();
+    }
+}
+
+function setupChart() {
+    var ctx = document.getElementById('main-chart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        responsive: true,
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+$(document).ready(function () {
+    showMenus();
+    $('#myLinks').hide();
+    $(window).resize(showMenus);
+
+    $('#hamburger').click(function () {
+        $('#myLinks').toggle();
+    });
+
+    setupChart();
+});
